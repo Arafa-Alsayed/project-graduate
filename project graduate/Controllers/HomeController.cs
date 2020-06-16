9ce6f46119f584f880a193efe778577b6b7abbfe
unknown <ArafaAlsayed@DESKTOP-VHA0M2N>
2020-06-16 19:02:42 +0200

    complete design

---
+++ project graduate/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using project_graduate.Models;
+
+namespace project_graduate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Text()
+        {
+            return View();
+        }
+        public IActionResult Indexfile()
+        {
+            return View();
+        }
+        public IActionResult DecryptionText()
+        {
+            return View();
+        }
+        public IActionResult DecryptionFile()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ project graduate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace project_graduate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
